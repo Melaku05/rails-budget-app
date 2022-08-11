2324ca67876264d81020e7cb74a151f5f7e79ee0
melaku05 <melakushiferaw05@gmail.com>
2022-08-11 12:25:27 +0300

    setup linter, and create home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+   def authenticate_user!
+      if user_signed_in?
+         redirect_to categories_path
+      end
+   end   
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :categories
+  unauthenticated :user do
+    root to: 'home#index'
+  end
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "home#index"
+ 
 end
